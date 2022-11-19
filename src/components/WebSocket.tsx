425e42ca4234d55b7f81c09b8b4053fe5fea5342
Ashleen <2552805d@student.gla.ac.uk>
2022-11-19 15:45:46 +0000

    fixed websocket to retirve price

--- src/components/WebSocket.tsx
+++ src/components/WebSocket.tsx
@@ -1,20 +1,27 @@
 import { FC, useEffect, useState } from "react";
+// import { WebSocketServer } from 'ws';
 
+const PriceDisplay: FC = () => {
+  const coin = "btcusdt";
 
-const PriceDisplay : FC = () => {
-    
-    useEffect(()=>{
-        console.log("seting up websocket")
-        let ws = new WebSocket('wss://stream.binance.com:9443/');
-        console.log(ws)
-        ws.onmessage = (event) => {
-            console.log(event)
-            // setPrice(JSON.parse(event.data));
-        }
-    },[])
+  useEffect(() => {
+    // initialise websocket
+    const ws = new WebSocket("wss://stream.bybit.com/contract/usdt/public/v3");
 
-    let [price, setPrice] = useState("0")
-    return <div>{price}</div>
-}
+    // connect to websocket and send request for BTC
+    ws.onopen = () => {
+      ws.send('{"op": "subscribe", "args": ["publicTrade.BTCUSDT"]}');
+    };
+
+    // retrieve BTC price on trade
+    ws.onmessage = (event) => {
+      const trade = JSON.parse(event.data);
+      setPrice(trade.data[0].p);
+    };
+  }, []);
+
+  let [price, setPrice] = useState("0");
+  return <div>{price}</div>;
+};
 
 export default PriceDisplay;
