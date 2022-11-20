fbfa0e2178c88a7e0c5543d706be52688c6b00d4
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-11-20 11:34:33 +0000

    deployed 30 mins before

---
+++ src/components/controllers/CandleController.tsx
@@ -0,0 +1,12 @@
+import Candlestick from "../charts/CandlestickChart";
+import Controller from "../Controller";
+
+const ChartController = () => {
+  return (
+    <Controller>
+      <Candlestick />
+    </Controller>
+  );
+};
+
+export default ChartController;
