cd16f854f2305233e47b3847264b62a52e196d08
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-11-20 00:16:47 +0000

    fixed dropdown z-index behaviour

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -17,21 +17,24 @@ const Dropdown: FC<props> = ({ tokens, state, setState }) => {
   };
 
   return (
-    <div className='grid gap-2'>
+    <div className="grid gap-2 relative">
+      {/* position: absolute;
+    transform: translate(0,80px);
+    z-index: 100; */}
       <div
         className={`p-6 w-80 h-max flex justify-between 
         rounded-lg bg-slate-800 hover:bg-slate-600 cursor-pointer`}
         onClick={() => setExpanded(!expanded)}
       >
-        <p className='text-white font-medium'>{state}</p>
+        <p className="text-white font-medium">{state}</p>
         {expanded ? <Up /> : <Down />}
       </div>
       {expanded && (
-        <div className='p-2 w-80 grid grid-cols-1 rounded-lg bg-slate-700 relative'>
+        <div className="p-2 w-80 grid grid-cols-1 rounded-lg bg-slate-700 absolute translate-y-[80px] z-10">
           {tokens.map((token) => (
             <div
               key={token}
-              className='p-4 rounded-lg hover:bg-gray-200/60 text-white hover:text-black hover:font-semibold cursor-pointer'
+              className="p-4 rounded-lg hover:bg-gray-200/60 text-white hover:text-black hover:font-semibold cursor-pointer"
               onClick={() => {
                 handleOption(token);
               }}
