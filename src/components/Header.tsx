9c685b111c66687ac627ee23b146ae7b72ece7d7
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-11-20 00:21:47 +0000

    updated header with new routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,18 @@ import Rook from "./icons/rook-icon";
 
 let Header: FC = () => {
   return (
-    <div className='py-6 px-16 w-full flex flex-row gap-6 justify-start items-center shadow-md text-xl font-bold'>
-      <div className='-mt-1'>
+    <div className="py-6 px-16 w-full flex flex-row gap-6 justify-start items-center shadow-md text-xl font-bold">
+      <div className="-mt-1">
         <Rook />
       </div>
-      <a href='/' className='hover:underline'>
+      <a href="/" className="hover:underline">
         <div>Home</div>
+      </a>
+      <a href="/line" className="hover:underline">
+        <div>Lines</div>
+      </a>
+      <a href="/bubble" className="hover:underline">
+        <div>Bubbles</div>
       </a>
     </div>
   );
