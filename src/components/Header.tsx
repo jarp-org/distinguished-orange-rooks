7480314303e3fe9ea9bed06e033b3ae9ec13ed64
Petros Kitazos <2526547k@student.gla.ac.uk>
2022-11-20 10:55:31 +0000

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,39 +6,39 @@ import BarChartIcon from "./icons/BarChartIcon";
 
 let Header: FC = () => {
   return (
-    <div className="py-6 px-16 w-full flex flex-row gap-10 justify-start items-center shadow-md text-xl font-bold">
-      <div className="-mt-1">
+    <div className='py-6 px-16 w-full flex flex-row gap-10 justify-start items-center shadow-md text-xl font-bold'>
+      <div className='-mt-1'>
         <Rook />
       </div>
-      <a href="/" className="-ml-3 hover:underline">
+      <a href='/' className='-ml-3 hover:underline'>
         <div>Home</div>
       </a>
       <a
-        href="/line"
-        className="hover:underline flex flex-row gap-2 items-center"
+        href='/line'
+        className='hover:underline flex flex-row gap-2 items-center'
       >
-        <div className="w-6 h-6">
+        <div className='w-6 h-6'>
           <LineChartIcon />
         </div>
-        <div>Lines</div>
+        <div>Line Chart</div>
       </a>
       <a
-        href="/bubble"
-        className="hover:underline  flex flex-row gap-2 items-centers"
+        href='/bubble'
+        className='hover:underline  flex flex-row gap-2 items-centers'
       >
-        <div className="w-6 h-6 -rotate-45">
+        <div className='w-6 h-6 -rotate-45'>
           <BubblesIcon />
         </div>
-        <div>Bubbles</div>
+        <div>Volume Chart</div>
       </a>
       <a
-        href="/bar-race"
-        className="hover:underline  flex flex-row gap-2 items-centers"
+        href='/bar-race'
+        className='hover:underline  flex flex-row gap-2 items-centers'
       >
-        <div className="w-6 h-6">
+        <div className='w-6 h-6'>
           <BarChartIcon />
         </div>
-        <div>Bubbles</div>
+        <div>Token Comparisons</div>
       </a>
     </div>
   );
--- src/components/charts/BubbleChart.tsx
+++ src/components/charts/BubbleChart.tsx
@@ -4,33 +4,6 @@ import useLiveFeed from "../../hooks/useLiveFeed";
 import { tokenContext } from "../Controller";
 import Loading from "../Loading";
 
-const options = {
-  colorAxis: { legend: { position: "none" } },
-  hAxis: {
-    title: "Time",
-    slantedText: true,
-    titleTextStyle: {
-      fontSize: 16,
-      bold: true,
-      italic: false,
-    },
-  },
-  vAxis: {
-    title: "Price ($)",
-    titleTextStyle: {
-      fontSize: 16,
-      bold: true,
-      italic: false,
-    },
-    viewWindowMode: "pretty",
-    viewWindow: { min: 16500, max: 16800 }, //todo fix this
-  },
-  colors: ["#ff6600"],
-  title: "Trade Volume",
-  legend: { position: "none" },
-  height: 400,
-};
-
 const buildRow = (newData: trade): (string | number)[] => {
   const newRow: (string | number)[] = ["", 0, 0, "", 0];
   newRow[1] = newData.time;
@@ -41,11 +14,38 @@ const buildRow = (newData: trade): (string | number)[] => {
 };
 
 const BubbleChart: FC = () => {
+  const options = {
+    colorAxis: { legend: { position: "none" } },
+    hAxis: {
+      title: "Time",
+      slantedText: true,
+      titleTextStyle: {
+        fontSize: 16,
+        bold: true,
+        italic: false,
+      },
+    },
+    vAxis: {
+      title: "Price ($)",
+      titleTextStyle: {
+        fontSize: 16,
+        bold: true,
+        italic: false,
+      },
+      viewWindowMode: "pretty",
+      viewWindow: { min: 16500, max: 16800 }, //todo fix this
+    },
+    colors: ["#ff6600"],
+    title: "Trade Volume",
+    legend: { position: "none" },
+    height: 400,
+  };
+
   let { subscription: currData, tokens } = useContext(tokenContext);
   const [sliderVal, setSliderVal] = useState(35);
   const [liveData, setLiveData] = useLiveFeed([], sliderVal);
-  const [minVal, setMinVal] = useState();
-  const [maxVal, setMaxVal] = useState();
+  const [minVal, setMinVal] = useState(currData[tokens[0]].price * 0.75);
+  const [maxVal, setMaxVal] = useState(currData[tokens[0]].price * 1.5);
 
   let [loading, setLoading] = useState(true);
 
@@ -60,6 +60,9 @@ const BubbleChart: FC = () => {
 
     tokens.forEach((token) => {
       temp.push(buildRow(currData[token]));
+      let currPrice = currData[token].price;
+      if (currPrice < minVal) setMinVal(currPrice);
+      if (currPrice > maxVal) setMaxVal(currPrice);
     });
 
     if (loading) setLoading(tokens.some((t) => currData[t].time === 0));
@@ -76,26 +79,26 @@ const BubbleChart: FC = () => {
   ) : (
     <>
       <Chart
-        chartType="BubbleChart"
-        width="100%"
-        height="400px"
+        chartType='BubbleChart'
+        width='100%'
+        height='400px'
         data={[["ID", "Time", "Price", "Token", "Quantity"], ...liveData]}
         options={options}
       />
-      <div className="mx-40 w-1/2">
+      <div className='mx-40 w-1/2'>
         <label
-          htmlFor="steps-range"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          htmlFor='steps-range'
+          className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
         />
         <input
-          id="steps-range"
-          type="range"
-          min="10"
-          max="60"
-          step="1"
+          id='steps-range'
+          type='range'
+          min='10'
+          max='60'
+          step='1'
           value={sliderVal}
           onChange={(e) => setSliderVal(parseInt(e.target.value))}
-          className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
         />
       </div>
     </>
